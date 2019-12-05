f1b8f06f46ad3f486436352e48fb5c6d805e6495
dmdeveloper <kudatawira@gmail.com>
2019-12-05 16:16:46 +0200

    add ct_vldt_trggr_tbl_vldtn_9 viewer

--- inst/htmlwidgets/PantheraIDSImageViewer.js
+++ inst/htmlwidgets/PantheraIDSImageViewer.js
@@ -47,14 +47,26 @@ HTMLWidgets.widget({
                 }
             );
         }
-        else if (x.componentID === "spcs_idntfctn_id_rf_1")
+
+        if (x.componentID === "ct_vldt_img_trggr_tbl_vldtn_9")
+        {
+          //console.log("ct_vldt_img_trggr_tbl_vldtn_9");
+          readServerData_ct_vldt(x.message);
+          Shiny.addCustomMessageHandler("ct_vldt_trggr_tbl_vldtn_9_button",
+                function(mesg) {
+                  console.log("Handler ct_vldt_trggr_tbl_vldtn_9_button");
+                  readServerData_ct_vldt(x.message);
+                }
+          );
+        }
+        /*else if (x.componentID === "spcs_idntfctn_id_rf_1")
         {
           fetchServerData(x.message);
         }
         else
         {
           fetchServerData_rf2(x.message);
-        }
+        }*/
 
       },
 
