0cca00a4a5cc5709f412b361d1150463249b2583
dmdeveloper <kudatawira@gmail.com>
2019-12-05 19:02:36 +0200

    Update PantheraIDSImageViewer.js

--- inst/htmlwidgets/PantheraIDSImageViewer.js
+++ inst/htmlwidgets/PantheraIDSImageViewer.js
@@ -51,7 +51,7 @@ HTMLWidgets.widget({
         if (x.componentID === "ct_vldt_img_trggr_tbl_vldtn_9")
         {
           //console.log("ct_vldt_img_trggr_tbl_vldtn_9");
-          //readServerData_ct_vldt(x.message);
+          readServerData_ct_vldt(x.message);
           Shiny.addCustomMessageHandler("ct_vldt_trggr_tbl_vldtn_9_button",
                 function(mesg) {
                   console.log("Handler ct_vldt_trggr_tbl_vldtn_9_button");
