8ac73b404414a8cd42ac526d1fb48af154a55ba5
dmdeveloper <kudatawira@gmail.com>
2020-04-20 06:41:06 +0200

    no-cache fetch

--- inst/htmlwidgets/lib/viewerHelpers.js
+++ inst/htmlwidgets/lib/viewerHelpers.js
@@ -1,14 +1,31 @@
 
     function fetchServerData(csvfile,moduleId)
-    { 
-        if(moduleId === "img_clssfctn_ud"){setImageArray(loadFile(csvfile))}
-        if(moduleId === "ct_vldt_img_trggr_tbl_vldtn_9") {setValidationArray(loadFile(csvfile),9)}
-        if(moduleId === "ct_vldt_img_trggr_tbl_vldtn_10"){setValidationArray(loadFile(csvfile),10)}
-        if(moduleId === "ct_vldt_img_trggr_tbl_vldtn_11"){setValidationArray(loadFile(csvfile),11)}
-        if(moduleId === "ct_vldt_img_trggr_tbl_vldtn_12"){setValidationArray(loadFile(csvfile),12)}
+    {
+        //if(moduleId === "img_clssfctn_ud"){setImageArray(loadFile(csvfile))}
+        if(moduleId === "img_clssfctn_ud"){ loadFile(csvfile).then(data => setImageArray(data))}
+        if(moduleId === "ct_vldt_img_trggr_tbl_vldtn_9") {loadFile(csvfile).then(data => setValidationArray(data),9)}
+        if(moduleId === "ct_vldt_img_trggr_tbl_vldtn_10"){loadFile(csvfile).then(data => setValidationArray(data),10)}
+        if(moduleId === "ct_vldt_img_trggr_tbl_vldtn_11"){loadFile(csvfile).then(data => setValidationArray(data),11)}
+        if(moduleId === "ct_vldt_img_trggr_tbl_vldtn_12"){loadFile(csvfile).then(data => setValidationArray(data),12)}
     }
 
-    function loadFile(filename) {
+    /*loadFile(csvfile).then(data => setValidationArray(data),9)
+    loadFile(csvfile).then(data => setValidationArray(data),10)
+    loadFile(csvfile).then(data => setValidationArray(data),11)
+    loadFile(csvfile).then(data => setValidationArray(data),12)
+    fetchServerData(file)
+      {
+        //console.log("In fetchServerData moduleId : " + this.moduleId);
+         //loadFile(file).then(data => this.readServerData(data));
+         if(moduleId === "img_clssfctn_ud"){
+            setImageArray(loadFile(csvfile));
+            loadFile(file).then(data => setImageArray(data));
+            
+
+        }
+      }*/
+
+   /* function loadFile(filename) {
         //console.log("In loadFile");
         let result = null;
         let xmlhttp = new XMLHttpRequest();
@@ -19,7 +36,7 @@
         return result;
         }
         return result;
-    }
+    }*/
 
     function observeClick(event)
     {
@@ -31,3 +48,18 @@
       //console.log("In clcsfcnSave()");
       saveButtonListerner();
     }
+
+
+
+    async function loadFile(filename) {
+        console.log("new loadFile");
+        let response = await fetch(filename,{cache: "no-cache"});
+                  //proceed once the first promise is resolved.
+          if(response.ok){
+            let data = await response.text();
+            //console.log("In new loadFile : " + data);
+            return (data.replace(/^\s*$[\n\r]{1,}/gm, ''));
+          }
+          return 0;
+    }
+      
