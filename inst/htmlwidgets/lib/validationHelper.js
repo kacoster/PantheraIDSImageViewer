ecbdeb693f0455bec3dda98722d948d0452b7f7a
dmdeveloper <kudatawira@gmail.com>
2020-05-13 09:14:22 +0200

    Update validationHelper.js

--- inst/htmlwidgets/lib/validationHelper.js
+++ inst/htmlwidgets/lib/validationHelper.js
@@ -7,7 +7,7 @@
  * ***************************************************************************/
 
 
-  var ct_vldt_img_trggr_tbl_vldtn_9  = new ViewerComponent(0,30,5,"ct_vldt_img_trggr_tbl_vldtn_9","img_clssfctn_ud.csv");
+  var ct_vldt_img_trggr_tbl_vldtn_9  = new ViewerComponent(0,30,5,"ct_vldt_img_trggr_tbl_vldtn_9","ct_vldt_img_trggr_tbl_vldtn_9.csv");
   var ct_vldt_img_trggr_tbl_vldtn_11 = new ViewerComponent(0,30,5,"ct_vldt_img_trggr_tbl_vldtn_11","ct_vldt_img_trggr_tbl_vldtn_11.csv");
   var ct_vldt_img_trggr_tbl_vldtn_10 = new ViewerComponent(0,30,5,"ct_vldt_img_trggr_tbl_vldtn_10","ct_vldt_img_trggr_tbl_vldtn_10.csv");
   var ct_vldt_img_trggr_tbl_vldtn_12 = new ViewerComponent(0,30,5,"ct_vldt_img_trggr_tbl_vldtn_12","ct_vldt_img_trggr_tbl_vldtn_12.csv");
